9d276c42fc3f8042c73aad35f9c3604118825810
Andrey <methos2013@gmail.com>
2018-06-07 17:17:15 +0300

    Add registration and login module

--- src/app/core/api-urls.ts
+++
@@ -1,5 +0,0 @@
-import { environment } from '../../environments/environment';
-
-export class ApiUrls {
-  static posts = `${environment.apiBase}/posts`;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  hmr: false,
-  apiBase: 'http://localhost:3030'
-};
